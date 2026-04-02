f55d703773c0fd15045cbe4fd22aa6c2ba55ec47
Joshua Tule <Joshtulz@gmail.com>
2026-04-02 14:59:14 +0100

    Initial Commit - Assignment 3

---
+++ HPlusSport/HPlusSport.API/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace HPlusSport.API.Models
+{
+    public class Product
+    {
+        
+        public int Id { get; set; }
+
+        
+        [Required]
+        public string Sku { get; set; } = string.Empty;
+
+        
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+    
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+       
+        public decimal Price { get; set; }
+
+        // Availability status of the product
+        public bool IsAvailable { get; set; }
+
+        // Foreign key for the Category entity, required
+        [Required]
+        public int CategoryId { get; set; }
+
+        // Navigation property for the Category, ignored during JSON serialization to prevent circular references
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+    }
+}
---
+++ HPlusSport/HPlusSport.API/Program.cs
@@ -0,0 +1,44 @@
+using HPlusSport.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add MySQL database
+builder.Services.AddDbContext<ShopContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+// Add CORS
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
